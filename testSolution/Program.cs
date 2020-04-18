f0fabb8f647f8321a63a7a4066ef65808b12a1a4
ZhangLinShui <hz0003234@163.com>
2020-04-18 23:14:45 +0800

    Update Program.cs
    多谢了几行Hello World

--- testSolution/Program.cs
+++ testSolution/Program.cs
@@ -7,6 +7,7 @@ namespace testSolution
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); Console.WriteLine("Hello World!"); Console.WriteLine("Hello World!");
         }
     }
 }
