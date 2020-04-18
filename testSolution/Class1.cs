9f6997e6712e3c615eefb08e25e4f36fe1e656c3
ZhangLinShui <hz0003234@163.com>
2020-04-18 23:20:28 +0800

    添加class1类

---
+++ testSolution/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testSolution
+{
+    class Class1
+    {
+        public void PrintMy()
+        {
+            Console.WriteLine("Class1      PrintMy()");
+        }
+    }
+}
--- testSolution/Program.cs
+++ testSolution/Program.cs
@@ -4,11 +4,14 @@ namespace testSolution
 {
     class Program
     {
+        static Class1 class1 = new Class1();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
             Console.WriteLine("你好  世界");
+            class1.PrintMy();
+            
         }
     }
 }
